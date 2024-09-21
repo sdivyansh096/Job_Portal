769ea34112b660c48c51c7cf63e652f3db46b893
sdivyanshhehe <sdivyansh096@gmail.com>
2024-09-21 17:26:00 +0530

    few changes

---
+++ client/src/components/AppliedJobTable.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const AppliedJobTable = () => {
+  return <div>AppliedJobTable</div>;
+};
+export default AppliedJobTable;
---
+++ client/src/components/JobDescription.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JobDescription = () => {
+  return <div>Job</div>;
+};
+
+export default JobDescription;
