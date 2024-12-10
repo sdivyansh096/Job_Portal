a64261c4116dc3aa004ae229ac0d771f9ba5a42f
sdivyanshhehe <sdivyansh096@gmail.com>
2024-12-10 22:48:50 +0530

    changes in frontend

---
+++ client/src/utils/constant.jsx
@@ -0,0 +1,8 @@
+//these are api end points whenever we call an api to get something from backend (we have build different routes in backend)
+//we will refer to this and get whatever we want from backend in frontend
+//as we have build logics in backend and connected it to monogdb where info is stored
+export const USER_API_END_POINT = "http://localhost:8000/ai/v1/user";
+export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const APPLICATION_API_END_POINT =
+  "http://localhost:8000/api/v1/application";
+export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -4,4 +4,6 @@ dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
